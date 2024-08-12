8098f643fd8c7ee273cb91e4f355d8014189c97a
SwartyPDX <davidswartwood@outlook.com>
2024-08-12 15:31:52 -0500

    Hating Pythin RN

---
+++ D206/churn/ChurnDetect.py
@@ -0,0 +1,29 @@
+#py version 3.12
+#import libraries
+import numpy as np
+import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
+
+#Load Data
+churn = pd.read_csv('D206\churn\churn_raw_data.csv')
+print(churn.head())
+
+#duplicate
+numunique=churn.nunique()
+numduplicates=churn.value_counts("CaseOrder")
+#ChurnDuplicates = churn.duplicated()
+
+print(numduplicates)
+print(numunique)
+
+
+
+
+#missing - null, NA and ""
+
+churn = pd.read_csv('churn_raw_data.csv')
+churn.head()
+msno.bar(churn)
+msno.matrix(churn, labels= True)
+#plt.show()
