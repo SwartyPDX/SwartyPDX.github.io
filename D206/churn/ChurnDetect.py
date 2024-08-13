74314eec1b522116aab569ffb63f2b987d4efab2
SwartyPDX <davidswartwood@outlook.com>
2024-08-12 20:38:49 -0500

    good rev, getting the visuals in.

--- D206/churn/ChurnDetect.py
+++ D206/churn/ChurnDetect.py
@@ -1,14 +1,21 @@
 #py version 3.12
 #import libraries
-import numpy as np
+
 import pandas as pd
 import missingno as msno
 import matplotlib.pyplot as plt
+import seaborn as sns
+import csv
 
-#Load Data
-churn = pd.read_csv('D206\churn\churn_raw_data.csv')
-#print(churn.head())
-
+#Load Datacd
+churn = pd.read_csv('churn_raw_data.csv')
+print(*churn)
+file = open("quantitative_vars.csv", "r")
+quantvars = list(csv.reader(file, delimiter=","))[0]
+file.close
+print(quantvars)
+quantChurn= churn.filter(quantvars)
+print(quantChurn.head())
 #duplicate
 numunique=churn.nunique()
 
@@ -21,4 +28,14 @@ print(numunique)
 #missing - null, NA and ""
 msno.bar(churn)
 msno.matrix(churn, labels= True)
-#plt.show()
+
+#Finding outliers
+plt.figure()
+#sns.boxplot(quantChurn)
+fig, axes = plt.subplots(1, len(quantvars))
+for i, col in enumerate(quantvars):
+    ax = sns.boxplot(y=quantChurn[col], ax=axes.flatten()[i])
+    ax.set_ylim(quantChurn[col].min(), quantChurn[col].max())
+    ax.set_ylabel(col + ' / Unit')
+plt.show()
+plt.show()
