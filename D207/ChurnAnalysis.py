c8be18515b55c041d4ee1b2f5c063bd7a72050a1
SwartyPDX <davidswartwood@outlook.com>
2024-10-30 20:25:44 -0500

    Templating #1

---
+++ D207/ChurnAnalysis.py
@@ -0,0 +1,24 @@
+#load libraries
+
+
+
+
+#Load data
+
+
+#Get Columns by type
+
+
+
+#Run Initial Analysis
+
+
+
+#Decision Pt
+
+
+#More detailed Analysis pending initial results
+
+
+
+#Final report
