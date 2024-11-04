934e90f0d605fd4c8f017e7364b43f9c1e9f835f
SwartyPDX <davidswartwood@outlook.com>
2024-11-03 19:55:11 -0500

    D207 Section B1

--- D207/ChurnAnalysis.py
+++ D207/ChurnAnalysis.py
@@ -1,24 +1,29 @@
 #load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages as PP
+import seaborn as sns
+import csv
+from scipy.stats import f_oneway
+import pingouin as pg
 
-
-
+np.set_printoptions(legacy='1.25')
 
 #Load data
-
-
+churn = pd.read_csv('D207/churn_clean.csv')
+#Code for Rubric B1
 #Get Columns by type
-
-
+age_pay=churn.groupby("PaymentMethod")["Age"].apply(list)
 
 #Run Initial Analysis
+anova=f_oneway(age_pay.iloc[0],age_pay.iloc[1],age_pay.iloc[2],age_pay.iloc[3])
+print(anova)
+
+
+#Code for Rubric C and C1
 
 
 
-#Decision Pt
 
-
-#More detailed Analysis pending initial results
-
-
-
-#Final report
+#Code for Rubric D1 and D2
